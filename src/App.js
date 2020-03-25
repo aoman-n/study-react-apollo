a70d6643f10f903473b4971d87ec5fa5d809cb97
aoba <ipbdipbd00@gmail.com>
2020-03-26 01:37:21 +0900

    setup react apollo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import client from './client'
+
+const ME = gql`
+  query me {
+    user(login: "laster18") {
+      name
+      avatarUrl
+    }
+  }
+`
 
 function App() {
   return (
-    <div>Hello, World!!</div>
+    <ApolloProvider client={client} >
+      <div>Hello, World!!</div>
+
+      <Query query={ME}>
+        {
+          ({ loading, error, data }) => {
+            if (loading) return `Loading...`
+            if (error) return `Error! ${error.message}`
+
+            return <div>{data.user.name}</div>
+          }
+        }
+      </Query>
+    </ApolloProvider>
   )
 }
 
